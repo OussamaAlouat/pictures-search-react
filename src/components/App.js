ef2b7da58e8b0806da1a5ab3edfd2eee3e7447a7
Oussama Alouat <al317257@uji.es>
2021-09-14 14:23:09 +0200

    Used unsplash to search the photos

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component  {
   onSearchSubmit(term) {
     console.log('Term: ', term)
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID 3a206d4d6f404f73ce5e0665c76f2c93d5dce476eaf6e420435f70d56f96fe1a'
+      }
+    }).then((response)  => {
+      console.log(response)
+    })
   }
 
   render () {
