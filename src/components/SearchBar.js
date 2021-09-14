7ee2c6eaf31804c3230199d2fb9d0d8bb1c327dc
Oussama Alouat <al317257@uji.es>
2021-09-14 13:02:01 +0200

    on change data, it will be saved on state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { item: '' }
+
+  onInputChange(event) {
+    const { value } = event.target
+    this.setState({ item: value })
+  }
+
   render () {
     return(
     <div className="ui fluid icon input">
-      <input type="text" placeholder="Search a very wide input..."/>
+      <input
+        type="text"
+        placeholder="Search an image ..."
+        onChange={(e) => this.onInputChange(e)}
+      />
       <i className="search icon"></i>
     </div>
     )
