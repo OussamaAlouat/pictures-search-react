afd178195b65485bf489401a27478171b5ad15cd
Oussama Alouat <al317257@uji.es>
2021-09-14 13:58:46 +0200

    Transform App in a class based component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div>
-      <div className="header-container">
-        <h1>React Picture Search</h1>
-      </div>
-      <div className="container">
-        <SearchBar></SearchBar>
+class App extends React.Component  {
+  render () {
+    return (
+      <div>
+        <div className="header-container">
+          <h1>React Picture Search</h1>
+        </div>
+        <div className="container">
+          <SearchBar></SearchBar>
 
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
